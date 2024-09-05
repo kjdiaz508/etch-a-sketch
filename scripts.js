8a4c56e424e6d0f9634b60be80ea4f5d0500b9d2
kjdiaz508 <diazkevin508@gmail.com>
2024-09-04 17:30:26 -0700

    prettyfied it a tiny bit, may do more later

--- scripts.js
+++ scripts.js
@@ -1,44 +1,41 @@
 
 const sizeBtn = document.querySelector("#size-button");
 sizeBtn.addEventListener("click", function (e) {
-    const newSize = parseInt(prompt("Select a new size for the grid.\nMax: 100"));
-
+    const newSize = parseInt(prompt("Select a new size for the grid.\nMax: 64"));
     if (Number.isNaN(newSize)) {
         alert("Enter a number please!");
-    } else if (newSize > 100){
-        alert("Too big! max size is 100.");
+    } else if (newSize > 64){
+        alert("Too big! max size is 64.");
     } else {
-        const container = document.querySelector(".container");
-        const oldGrid = document.querySelector(".grid");
-        container.removeChild(oldGrid);
         generateGrid(newSize);
     }
-    
 });
 
 function generateGrid(gridSize){
     const container = document.querySelector(".container");
-    const gridDimensions = 600;
-    const grid = document.createElement("div");
-    grid.setAttribute("class", "grid");
+    container.textContent = '';
+    const gridDimensions = container.clientWidth;
 
-    let squareDimensions = Math.floor(gridDimensions / gridSize);
-    grid.style.width = `${squareDimensions * gridSize}px`;
+    let squareDimensions = gridDimensions / gridSize;
 
-    for (let i=0; i < gridSize*gridSize; i++) {
-        const square = document.createElement("div");
-        square.setAttribute("class", "square");
-        square.style.cssText = `width: ${squareDimensions}px; height: ${squareDimensions}px;`
-        grid.appendChild(square);
+    for (let i=0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        for (let j=0; j < gridSize; j++){
+            const square = document.createElement("div");
+            square.setAttribute("class", "square");
+            square.style.cssText = `width: ${squareDimensions}px; height: ${squareDimensions}px;`
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+        row.addEventListener("mouseover", function (e) {
+            if (e.target.getAttribute("class") == "square"){
+                e.target.style.backgroundColor = "black";
+            }
+        });
     }
 
-    container.appendChild(grid);
-
-    grid.addEventListener("mouseover", function (e) {
-        if (e.target.getAttribute("class") == "square"){
-            e.target.style.backgroundColor = "black";
-        }
-    });
+    
 
 }
 
